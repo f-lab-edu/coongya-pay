34fa6147026ad1005604e1b59d9ec36c9be937a9
coongya <moominie18@gmail.com>
2026-04-30 01:28:35 +0900

    feat: 도메인과 영속성 계층 분리를 위한 repository 추가

---
+++ src/main/java/com/flab/coongyapay/user/service/SignupService.java
@@ -0,0 +1,27 @@
+package com.flab.coongyapay.user.service;
+
+import com.flab.coongyapay.user.controller.dto.SignupRequest;
+
+public class SignupService {
+
+    public void signup(SignupRequest request) {
+
+        // 1. 이메일 중복 검사
+
+        // 2. KYC
+
+        // 3. 비밀번호 단방향 해시
+
+        // 4. 송금 비밀번호 단방향 해시
+
+        // 5. 이메일 중복 검사(비관적 락)
+
+        // 6. user 저장
+
+        // 7. user_password 저장
+
+        // 8. wallet 저장
+
+        // 9. user_transfer_pin 저장
+    }
+}
