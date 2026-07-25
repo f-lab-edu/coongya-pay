b2a97e0e27f676e25217e87d5b6ccf9f0cd1d227
coongya <moominie18@gmail.com>
2026-07-25 15:55:58 +0900

    feat: 본인 소유 지갑 조회 추가

--- src/main/java/com/flab/coongyapay/wallet/mapper/WalletMapper.java
+++ src/main/java/com/flab/coongyapay/wallet/mapper/WalletMapper.java
@@ -2,9 +2,16 @@ package com.flab.coongyapay.wallet.mapper;
 
 import com.flab.coongyapay.wallet.mapper.dto.WalletDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Optional;
 
 @Mapper
 public interface WalletMapper {
 
     void insert(WalletDto walletDto);
+
+    Optional<WalletDto> findByUserId(@Param("userId") Long userId);
+
+    Optional<WalletDto> findByUserIdForUpdate(@Param("userId") Long userId);
 }
--- src/main/java/com/flab/coongyapay/wallet/repository/WalletRepository.java
+++ src/main/java/com/flab/coongyapay/wallet/repository/WalletRepository.java
@@ -7,6 +7,8 @@ import com.flab.coongyapay.wallet.mapper.dto.WalletDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class WalletRepository {
@@ -18,5 +20,13 @@ public class WalletRepository {
         WalletDto walletDto = walletAssembler.toDto(wallet);
         walletMapper.insert(walletDto);
         return walletAssembler.toDomain(walletDto);
+    }
+
+    public Optional<Wallet> findByUserId(Long userId) {
+        return walletMapper.findByUserId(userId).map(walletAssembler::toDomain);
+    }
+
+    public Optional<Wallet> findByUserIdForUpdate(Long userId) {
+        return walletMapper.findByUserIdForUpdate(userId).map(walletAssembler::toDomain);
     }
 }
